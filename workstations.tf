2fac6c30aad4008189c1bcf24505a26ac121cb32
Dave Farinelli <d@dfar.io>
2026-05-09 17:45:35 +0000

    feat: Add TF validate step, workstation

--- workstations.tf
+++ workstations.tf
@@ -16,6 +16,24 @@ variable "workstations_subnetwork_name" {
   default     = "default"
 }
 
+variable "workstations_config_id" {
+  description = "ID for the Workstations configuration."
+  type        = string
+  default     = "default-workstations-config"
+}
+
+variable "workstations_machine_type" {
+  description = "Machine type for workstation hosts."
+  type        = string
+  default     = "e2-standard-4"
+}
+
+variable "workstation_id" {
+  description = "ID for the individual workstation instance."
+  type        = string
+  default     = "default-workstation"
+}
+
 data "google_compute_network" "workstations" {
   name = var.workstations_network_name
 }
@@ -39,4 +57,40 @@ resource "google_workstations_workstation_cluster" "default" {
   }
 
   depends_on = [google_project_service.enabled]
+}
+
+resource "google_workstations_workstation_config" "default" {
+  provider                = google-beta
+  project                 = var.gcp_project_id
+  location                = var.gcp_region
+  workstation_cluster_id  = google_workstations_workstation_cluster.default.workstation_cluster_id
+  workstation_config_id   = var.workstations_config_id
+
+  idle_timeout    = "1200s"
+  running_timeout = "43200s"
+
+  host {
+    gce_instance {
+      machine_type                = var.workstations_machine_type
+      boot_disk_size_gb           = 100
+      disable_public_ip_addresses = true
+    }
+  }
+
+  container {
+    image = "us-central1-docker.pkg.dev/cloud-workstations-images/predefined/code-oss:latest"
+  }
+
+  depends_on = [google_workstations_workstation_cluster.default]
+}
+
+resource "google_workstations_workstation" "default" {
+  provider                = google-beta
+  project                 = var.gcp_project_id
+  location                = var.gcp_region
+  workstation_cluster_id  = google_workstations_workstation_cluster.default.workstation_cluster_id
+  workstation_config_id   = google_workstations_workstation_config.default.workstation_config_id
+  workstation_id          = var.workstation_id
+
+  depends_on = [google_workstations_workstation_config.default]
 }
