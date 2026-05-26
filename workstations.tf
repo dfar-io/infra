257e58483e2d9a461df6c130b1ff4fbd24da765c
Dave Farinelli <d@dfar.io>
2026-05-26 22:48:01 +0000

    Remove G Workstations

--- workstations.tf
+++ workstations.tf
@@ -43,54 +43,54 @@ data "google_compute_subnetwork" "workstations" {
   region = var.gcp_region
 }
 
-resource "google_workstations_workstation_cluster" "default" {
-  provider               = google-beta
-  project                = var.gcp_project_id
-  location               = var.gcp_region
-  workstation_cluster_id = var.workstations_cluster_id
-
-  network    = data.google_compute_network.workstations.id
-  subnetwork = data.google_compute_subnetwork.workstations.id
-
-  labels = {
-    managed-by = "terraform"
-  }
-
-  depends_on = [google_project_service.enabled]
-}
-
-resource "google_workstations_workstation_config" "default" {
-  provider                = google-beta
-  project                 = var.gcp_project_id
-  location                = var.gcp_region
-  workstation_cluster_id  = google_workstations_workstation_cluster.default.workstation_cluster_id
-  workstation_config_id   = var.workstations_config_id
-
-  idle_timeout    = "1200s"
-  running_timeout = "43200s"
-
-  host {
-    gce_instance {
-      machine_type                = var.workstations_machine_type
-      boot_disk_size_gb           = 100
-      disable_public_ip_addresses = true
-    }
-  }
-
-  container {
-    image = "us-central1-docker.pkg.dev/cloud-workstations-images/predefined/code-oss:latest"
-  }
-
-  depends_on = [google_workstations_workstation_cluster.default]
-}
-
-resource "google_workstations_workstation" "default" {
-  provider                = google-beta
-  project                 = var.gcp_project_id
-  location                = var.gcp_region
-  workstation_cluster_id  = google_workstations_workstation_cluster.default.workstation_cluster_id
-  workstation_config_id   = google_workstations_workstation_config.default.workstation_config_id
-  workstation_id          = var.workstation_id
-
-  depends_on = [google_workstations_workstation_config.default]
-}
+# resource "google_workstations_workstation_cluster" "default" {
+#   provider               = google-beta
+#   project                = var.gcp_project_id
+#   location               = var.gcp_region
+#   workstation_cluster_id = var.workstations_cluster_id
+#
+#   network    = data.google_compute_network.workstations.id
+#   subnetwork = data.google_compute_subnetwork.workstations.id
+#
+#   labels = {
+#     managed-by = "terraform"
+#   }
+#
+#   depends_on = [google_project_service.enabled]
+# }
+#
+# resource "google_workstations_workstation_config" "default" {
+#   provider                = google-beta
+#   project                 = var.gcp_project_id
+#   location                = var.gcp_region
+#   workstation_cluster_id  = google_workstations_workstation_cluster.default.workstation_cluster_id
+#   workstation_config_id   = var.workstations_config_id
+#
+#   idle_timeout    = "1200s"
+#   running_timeout = "43200s"
+#
+#   host {
+#     gce_instance {
+#       machine_type                = var.workstations_machine_type
+#       boot_disk_size_gb           = 100
+#       disable_public_ip_addresses = true
+#     }
+#   }
+#
+#   container {
+#     image = "us-central1-docker.pkg.dev/cloud-workstations-images/predefined/code-oss:latest"
+#   }
+#
+#   depends_on = [google_workstations_workstation_cluster.default]
+# }
+#
+# resource "google_workstations_workstation" "default" {
+#   provider                = google-beta
+#   project                 = var.gcp_project_id
+#   location                = var.gcp_region
+#   workstation_cluster_id  = google_workstations_workstation_cluster.default.workstation_cluster_id
+#   workstation_config_id   = google_workstations_workstation_config.default.workstation_config_id
+#   workstation_id          = var.workstation_id
+#
+#   depends_on = [google_workstations_workstation_config.default]
+# }
