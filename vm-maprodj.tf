4b4f18d18df33666d6e2dfb1796968f6301d65e8
Dave Farinelli <d@dfar.io>
2026-06-06 20:42:56 +0000

    feat: maprodj inits with WP dependencies, docs

--- vm-maprodj.tf
+++ vm-maprodj.tf
@@ -17,11 +17,38 @@ resource "google_compute_instance" "maprodj" {
     access_config {}
   }
 
-  tags = ["maprodj", "ssh"]
+  tags = ["maprodj", "ssh", "http-server", "https-server"]
 
   metadata = {
     enable-oslogin = "TRUE"
   }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    set -euxo pipefail
+
+    MARKER_FILE="/var/lib/maprodj-startup.done"
+    if [[ -f "$MARKER_FILE" ]]; then
+      systemctl start nginx || true
+      exit 0
+    fi
+
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update
+    apt-get install -y ca-certificates curl lsb-release gnupg2 zip nginx mariadb-server
+
+    curl -fsSL https://packages.sury.org/php/apt.gpg | gpg --dearmor -o /usr/share/keyrings/sury-php.gpg
+    echo "deb [signed-by=/usr/share/keyrings/sury-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/sury-php.list
+
+    apt-get update
+    apt-get install -y php7.4-fpm php7.4-mysql php7.4-cli php7.4-curl php7.4-gd php7.4-mbstring php7.4-xml php7.4-zip
+
+    systemctl enable --now nginx
+    systemctl enable --now mariadb
+    systemctl enable --now php7.4-fpm
+
+    touch "$MARKER_FILE"
+  EOT
 
   labels = {
     managed_by = "terraform"
@@ -29,18 +56,4 @@ resource "google_compute_instance" "maprodj" {
   }
 
   allow_stopping_for_update = true
-}
-
-resource "google_compute_firewall" "maprodj_allow_http_https" {
-  name    = "maprodj-allow-http-https"
-  network = "default"
-
-  direction     = "INGRESS"
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["maprodj"]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
 }
